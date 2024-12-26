cfb24bb4295b30d6f85a0dcee912c508e5f7f940
Suraj Ravindra Dhumal <srdhumal2002@gmail.com>
2024-12-26 17:10:35 +0530

    Add project files.

---
+++ MvcPlusWebApiApp/Controllers/ValuesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcPlusWebApiApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("Hi")]
+        public IActionResult Hi()
+        {
+            return Ok("Hello");
+        }
+
+        [HttpGet("Data")]
+        public IActionResult Data()
+        {
+            return NotFound("User data Not Found");
+        }
+    }
+}
